c81ae626c45b1650efb18dc8f92190f1d787b2a3
sugawara-riku <sugariku0303@outlook.jp>
2022-12-24 18:11:19 +0000

    follow, unfollow

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -2,7 +2,7 @@
     {{-- ユーザ一覧ページへのリンク --}}
     <li><a class="link link-hover" href="{{ route('users.index') }}">Users</a></li>
     {{-- ユーザ詳細ページへのリンク --}}
-    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li><a class="link link-hover" href="{{ route('users.show', Auth::user()->id) }}">{{ Auth::user()->name }}&#39;s profile</a></li>
     <li class="divider lg:hidden"></li>
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
